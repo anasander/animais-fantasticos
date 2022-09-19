218c971b96be0b9404e768a4de4988a85cfb5fdd
Ana Sander <anagsander@gmail.com>
2022-09-19 18:23:27 -0300

    TAB nav

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 // Acessibilidade, tecla t aumenta o texto em 150%
 function handleClickT(event) {
-  //   console.log(event.key);
   if (event.key === "t") {
     document.documentElement.classList.toggle("textomaior");
   }
 }
 
 window.addEventListener("keydown", handleClickT);
+
+// TAB nav
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+if (tabMenu.length && tabContent.length) {
+  // iniciar a página com uma tab ativa
+  tabContent[0].classList.add("ativo");
+
+  function tabAtivo(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      tabAtivo(index);
+    });
+  });
+}
